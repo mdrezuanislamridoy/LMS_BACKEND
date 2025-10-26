987076f1315af1728a482eb5ef095a64a6d2a75a
mdrezuanislamridoy <mdrezuanislamridoy@gmail.com>
2025-10-26 13:08:05 +0600

    generate token file fixed

--- dist/utils/generateToken.d.ts
+++ dist/utils/generateToken.d.ts
@@ -1,7 +1,8 @@
+import { type SignOptions } from "jsonwebtoken";
 interface JwtPayload {
     id: string;
     role: "student" | "mentor" | "admin";
 }
-export declare const generateToken: (payload: JwtPayload, expiresIn?: string | number) => string;
+export declare const generateToken: (payload: JwtPayload, expiresIn?: SignOptions["expiresIn"]) => string;
 export {};
 //# sourceMappingURL=generateToken.d.ts.map
--- src/utils/generateToken.ts
+++ src/utils/generateToken.ts
@@ -1,16 +1,17 @@
-import jwt, { type Secret, type SignOptions } from "jsonwebtoken";
+import jwt, { type SignOptions } from "jsonwebtoken";
 import { env } from "../config/env.js";
 
 interface JwtPayload {
   id: string;
   role: "student" | "mentor" | "admin";
 }
+
 export const generateToken = (
   payload: JwtPayload,
-  expiresIn: string | number = "1h"
+  expiresIn: SignOptions["expiresIn"] = "1h"
 ): string => {
-  const secret: Secret = env.jwt_secret as string;
-
+  const secret = env.jwt_secret as string;
   const options: SignOptions = { expiresIn };
+
   return jwt.sign(payload, secret, options);
 };
