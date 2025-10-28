4ac9c401a24c0b01908464e06d01a78c5e358150
mdrezuanislamridoy <mdrezuanislamridoy@gmail.com>
2025-10-28 13:28:38 +0600

    daf

--- src/config/db.ts
+++ src/config/db.ts
@@ -2,11 +2,12 @@ import mongoose from "mongoose";
 import { env } from "./env.js";
 
 export const db = async () => {
-    try {
-        await mongoose.connect(env.db_url as string);
-        console.log("DB Connected");
-    } catch (error) {
-        console.log(error);
-    }
+  try {
+    await mongoose.connect(
+      "mongodb+srv://mdrezuanislamridoy:RRRidoy781@rrcluster.dzwno.mongodb.net/RR_LMS?retryWrites=true&w=majority&appName=RRCluster"
+    );
+    console.log("DB Connected");
+  } catch (error) {
+    console.log(error);
+  }
 };
-
